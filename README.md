c3a3687d3124c792143168a0ca83002d0c4d5261
hashashin <gentoo.power@gmail.com>
2020-01-28 01:08:05 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+**BeeNames**
+
+Simple Bukkit plugin to auto name bees that belongs to a player. It checks if the bee lives in a hive or a nest, 
+if it's a hive the plugin will name it randomly from a list of 200 names.
---
+++ src/main/java/me/hashashin/BeeNames/BeeNames.java
@@ -0,0 +1,113 @@
+/*
+BeeNames, simple bukkit plugin to auto name bees that live in a bee hive.
+    Copyright (C) 2020  hashashin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+package me.hashashin.BeeNames;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Bee;
+import org.bukkit.entity.EntityType;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.util.List;
+import java.util.Random;
+
+
+public class BeeNames extends JavaPlugin implements Listener {
+
+	@Override
+	public void onEnable() {
+		// Define the config.yml
+		final File check = new File(this.getDataFolder(), "config.yml");
+		// Check for existing config.yml, if not create it
+		if (!check.exists()) {
+			this.saveDefaultConfig();
+			this.reloadConfig();
+		}
+		// Register events
+		this.getServer().getPluginManager().registerEvents(this, this);
+		// Log loading
+		this.getLogger().info("Loaded " + this.getDescription().getName() +
+				" v" + this.getDescription().getVersion());
+	}
+
+	@Override
+	public void onDisable() {
+		// Log disabling
+		this.getLogger().info("Disabled " + this.getDescription().getName() +
+				" v" + this.getDescription().getVersion());
+	}
+
+	@EventHandler(priority = EventPriority.LOWEST)
+	public void onCreatureSpawn(CreatureSpawnEvent event) {
+		if(event.isCancelled()) {
+			return;
+		}
+		// Check if Creature is a BEE
+		if(event.getEntityType() == EntityType.BEE) {
+			if (this.getConfig().getBoolean("debug")) {
+				this.getLogger().info(event.getSpawnReason().name());
+			}
+			// Check if the bee spawn reason was a beehive, then check if already has a name
+			if (event.getSpawnReason().name().equals("BEEHIVE") && event.getEntity().getCustomName() != null) {
+				if (this.getConfig().getBoolean("debug")) {
+					this.getLogger().info("This bee already has a name: " + event.getEntity().getCustomName());
+				}
+				return;
+			}
+			// Check if event is instance of Bee
+			if(event.getEntity() instanceof org.bukkit.entity.Bee) {
+				// Check if the Bee lives in the wild
+				if (((Bee) event.getEntity()).getHive() == null) {
+					return;
+				} else //noinspection ConstantConditions
+					if (((Bee) event.getEntity()).getHive().getBlock().getBlockData().getMaterial()
+						== Material.BEE_NEST) {
+					if (this.getConfig().getBoolean("debug")) {
+						this.getLogger().info("This bee belongs to a nest.");
+					}
+					return;
+				}
+				// Load the BEE names out of config.yml into a List
+				List<?> names = this.getConfig().getList("names");
+				// Pick a random name from that list
+				String random_name = null;
+				if (names != null) {
+					Random rand = new Random();
+					random_name = names.get(rand.nextInt(names.size())).toString();
+				}
+
+				// Set the custom name of the Bee
+				event.getEntity().setCustomName(random_name);
+				event.getEntity().setCustomNameVisible(true);
+				
+				// Check if log mode is enabled
+				if (this.getConfig().getBoolean("debug")) {
+					// Log the actions
+					this.getLogger().info("Bee '"+ random_name +"' spawned at location: '"
+							+ event.getLocation().getWorld().toString()
+							+ "," + event.getLocation().getX() + ","
+							+ event.getLocation().getY() + "," + event.getLocation().getZ() +"'");
+				}
+			}
+		}
+	}
+}
